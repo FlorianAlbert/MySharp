b25c0b8502e279196f2088c6e8d1b52103379e7c
FlorianAlbert <61190722+FlorianAlbert@users.noreply.github.com>
2026-01-17 00:27:56 +0100

    Add SyntaxFacts round-trip tokenization test for all SyntaxKinds

---
+++ FlorianAlbert.MySharp.Sdk.Parser.Test/CodeAnalysis/Syntax/SyntaxFactsTests.cs
@@ -0,0 +1,31 @@
+﻿using FlorianAlbert.MySharp.Sdk.Parser.CodeAnalysis.Syntax;
+
+namespace FlorianAlbert.MySharp.Sdk.Parser.Test.CodeAnalysis.Syntax;
+
+public class SyntaxFactsTests
+{
+    [Theory]
+    [MemberData(nameof(GetAllSyntaxKinds))]
+    public void SyntaxFacts_GetText_RoundTrips(SyntaxKind kind)
+    {
+        string? text = SyntaxFacts.GetText(kind);
+
+        if (text is null)
+        {
+            return;
+        }
+
+        IEnumerable<SyntaxToken> tokens = SyntaxTree.ParseTokens(text);
+
+        SyntaxToken token = Assert.Single(tokens);
+
+        Assert.Equal(kind, token.Kind);
+        Assert.Equal(text, token.Text);
+    }
+
+    public static TheoryData<SyntaxKind> GetAllSyntaxKinds()
+    {
+        TheoryData<SyntaxKind> data = [.. Enum.GetValues<SyntaxKind>()];
+        return data;
+    }
+}
