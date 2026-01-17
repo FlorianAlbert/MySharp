04866792ef11fac1f5fa8129d4ce5c67d96924ea
FlorianAlbert <61190722+FlorianAlbert@users.noreply.github.com>
2026-01-17 11:27:24 +0100

    Remove redundant using directive from Lexer class

--- FlorianAlbert.MySharp.Sdk.Parser/CodeAnalysis/Syntax/Lexer.cs
+++ FlorianAlbert.MySharp.Sdk.Parser/CodeAnalysis/Syntax/Lexer.cs
@@ -1,6 +1,4 @@
-﻿using FlorianAlbert.MySharp.Sdk.Parser.CodeAnalysis;
-
-namespace FlorianAlbert.MySharp.Sdk.Parser.CodeAnalysis.Syntax;
+﻿namespace FlorianAlbert.MySharp.Sdk.Parser.CodeAnalysis.Syntax;
 
 internal sealed class Lexer
 {
