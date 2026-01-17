47b191117e13f216547a102a4658aa78aeb6d1af
FlorianAlbert <61190722+FlorianAlbert@users.noreply.github.com>
2026-01-17 13:17:54 +0100

    Refactor evaluator to use switch expression on `BoundNodeKind`

--- FlorianAlbert.MySharp.Sdk.Parser/CodeAnalysis/Evaluator.cs
+++ FlorianAlbert.MySharp.Sdk.Parser/CodeAnalysis/Evaluator.cs
@@ -20,73 +20,80 @@ internal sealed class Evaluator
 
     private object? EvaluateExpression(BoundExpression expression)
     {
-        if (expression is BoundLiteralExpression boundLiteralExpression &&
-            boundLiteralExpression.Value is not null)
+        return expression.Kind switch
         {
-            return boundLiteralExpression.Value;
-        }
+            BoundNodeKind.LiteralExpression => EvaluateLiteralExpression((BoundLiteralExpression) expression),
+            BoundNodeKind.VariableExpression => EvaluateVariableExpression((BoundVariableExpression) expression),
+            BoundNodeKind.AssignmentExpression => EvaluateAssignmentExpression((BoundAssignmentExpression) expression),
+            BoundNodeKind.UnaryExpression => EvaluateUnaryExpression((BoundUnaryExpression) expression),
+            BoundNodeKind.BinaryExpression => EvaluateBinaryExpression((BoundBinaryExpression) expression),
+            _ => throw new Exception($"Unexpected node {expression.Kind}")
+        };
+    }
 
-        if (expression is BoundVariableExpression boundVariableExpression)
+    private static object? EvaluateLiteralExpression(BoundLiteralExpression boundLiteralExpression)
+    {
+        return boundLiteralExpression.Value;
+    }
+
+    private object? EvaluateAssignmentExpression(BoundAssignmentExpression boundAssignmentExpression)
+    {
+        object? value = EvaluateExpression(boundAssignmentExpression.Expression);
+        _variables[boundAssignmentExpression.VariableSymbol] = value;
+
+        return value;
+    }
+
+    private object? EvaluateVariableExpression(BoundVariableExpression boundVariableExpression)
+    {
+        return _variables[boundVariableExpression.VariableSymbol];
+    }
+
+    private object? EvaluateUnaryExpression(BoundUnaryExpression boundUnaryExpression)
+    {
+        object? operand = EvaluateExpression(boundUnaryExpression.Operand);
+
+        return boundUnaryExpression.Operator.Kind switch
         {
-            return _variables[boundVariableExpression.VariableSymbol];
-        }
+            BoundUnaryOperatorKind.Identity => operand,
+            BoundUnaryOperatorKind.Negation => -(int?) operand,
+            BoundUnaryOperatorKind.LogicalNegation => !(bool?) operand,
+            _ => throw new Exception($"Unexpected unary operator {boundUnaryExpression.Operator.Kind}"),
+        };
+    }
 
-        if (expression is BoundAssignmentExpression boundAssignmentExpression)
-        {
-            object? value = EvaluateExpression(boundAssignmentExpression.Expression);
-            _variables[boundAssignmentExpression.VariableSymbol] = value;
-
-            return value;
-        }
-
-        if (expression is BoundUnaryExpression boundUnaryExpression)
-        {
-            object? operand = EvaluateExpression(boundUnaryExpression.Operand);
-
-            return boundUnaryExpression.Operator.Kind switch
-            {
-                BoundUnaryOperatorKind.Identity => operand,
-                BoundUnaryOperatorKind.Negation => -(int?) operand,
-                BoundUnaryOperatorKind.LogicalNegation => !(bool?) operand,
-                _ => throw new Exception($"Unexpected unary operator {boundUnaryExpression.Operator.Kind}"),
-            };
-        }
-
-        if (expression is BoundBinaryExpression boundBinaryExpression)
-        {
-            object? leftValue = EvaluateExpression(boundBinaryExpression.Left);
-            object? rightValue = EvaluateExpression(boundBinaryExpression.Right);
+    private object? EvaluateBinaryExpression(BoundBinaryExpression boundBinaryExpression)
+    {
+        object? leftValue = EvaluateExpression(boundBinaryExpression.Left);
+        object? rightValue = EvaluateExpression(boundBinaryExpression.Right);
 
 #pragma warning disable IDE0066 // Convert switch statement to expression
-            switch (boundBinaryExpression.Operator.Kind)
-            {
-                case BoundBinaryOperatorKind.Addition:
-                    return (int?) leftValue + (int?) rightValue;
-                case BoundBinaryOperatorKind.Subtraction:
-                    return (int?) leftValue - (int?) rightValue;
-                case BoundBinaryOperatorKind.Multiplication:
-                    return (int?) leftValue * (int?) rightValue;
-                case BoundBinaryOperatorKind.Division:
-                    return (int?) leftValue / (int?) rightValue;
-                case BoundBinaryOperatorKind.Modulo:
-                    return (int?) leftValue % (int?) rightValue;
-                case BoundBinaryOperatorKind.LogicalAnd:
-                    return (bool) leftValue! && (bool) rightValue!;
-                case BoundBinaryOperatorKind.LogicalOr:
-                    return (bool) leftValue! || (bool) rightValue!;
-                case BoundBinaryOperatorKind.BitwiseExclusiveOr:
-                    return EvaluateBitwiseExclusiveOr(leftValue, rightValue);
-                case BoundBinaryOperatorKind.Equals:
-                    return Equals(leftValue, rightValue);
-                case BoundBinaryOperatorKind.NotEquals:
-                    return !Equals(leftValue, rightValue);
-                default:
-                    throw new Exception($"Unexpected binary operator {boundBinaryExpression.Operator.Kind}");
-            }
-#pragma warning restore IDE0066 // Convert switch statement to expression
+        switch (boundBinaryExpression.Operator.Kind)
+        {
+            case BoundBinaryOperatorKind.Addition:
+                return (int?) leftValue + (int?) rightValue;
+            case BoundBinaryOperatorKind.Subtraction:
+                return (int?) leftValue - (int?) rightValue;
+            case BoundBinaryOperatorKind.Multiplication:
+                return (int?) leftValue * (int?) rightValue;
+            case BoundBinaryOperatorKind.Division:
+                return (int?) leftValue / (int?) rightValue;
+            case BoundBinaryOperatorKind.Modulo:
+                return (int?) leftValue % (int?) rightValue;
+            case BoundBinaryOperatorKind.LogicalAnd:
+                return (bool) leftValue! && (bool) rightValue!;
+            case BoundBinaryOperatorKind.LogicalOr:
+                return (bool) leftValue! || (bool) rightValue!;
+            case BoundBinaryOperatorKind.BitwiseExclusiveOr:
+                return EvaluateBitwiseExclusiveOr(leftValue, rightValue);
+            case BoundBinaryOperatorKind.Equals:
+                return Equals(leftValue, rightValue);
+            case BoundBinaryOperatorKind.NotEquals:
+                return !Equals(leftValue, rightValue);
+            default:
+                throw new Exception($"Unexpected binary operator {boundBinaryExpression.Operator.Kind}");
         }
-
-        throw new Exception($"Unexpected node {expression.Kind}");
+#pragma warning restore IDE0066 // Convert switch statement to expression
     }
 
     private static object? EvaluateBitwiseExclusiveOr(object? leftValue, object? rightValue)
