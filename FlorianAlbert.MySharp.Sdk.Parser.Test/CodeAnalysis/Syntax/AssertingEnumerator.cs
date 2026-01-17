bb75fc32013a6575d162624ef5ddb9a6b48db3c6
FlorianAlbert <61190722+FlorianAlbert@users.noreply.github.com>
2026-01-17 01:11:24 +0100

    Add parser tests for binary operators and add AssertingEnumerator utility for syntax tree assertions

---
+++ FlorianAlbert.MySharp.Sdk.Parser.Test/CodeAnalysis/Syntax/AssertingEnumerator.cs
@@ -0,0 +1,73 @@
+﻿using FlorianAlbert.MySharp.Sdk.Parser.CodeAnalysis.Syntax;
+
+namespace FlorianAlbert.MySharp.Sdk.Parser.Test.CodeAnalysis.Syntax;
+
+internal class AssertingEnumerator : IDisposable
+{
+    private readonly IEnumerator<SyntaxNode> _enumerator;
+    private bool _hasErrors;
+
+    public AssertingEnumerator(SyntaxNode node)
+    {
+        _enumerator = Flatten(node).GetEnumerator();
+    }
+
+    private static IEnumerable<SyntaxNode> Flatten(SyntaxNode node)
+    {
+        yield return node;
+        foreach (SyntaxNode child in node.GetChildren())
+        {
+            foreach (SyntaxNode descendant in Flatten(child))
+            {
+                yield return descendant;
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        if (!_hasErrors)
+        {
+            Assert.False(_enumerator.MoveNext(), "Expected no more nodes.");
+        }
+
+        _enumerator.Dispose();
+    }
+
+    public void AssertToken(SyntaxKind kind, string text)
+    {
+        try
+        {
+            Assert.True(_enumerator.MoveNext(), "Expected more nodes.");
+
+            Assert.Equal(kind, _enumerator.Current.Kind);
+
+            SyntaxToken token = Assert.IsType<SyntaxToken>(_enumerator.Current);
+            Assert.Equal(text, token.Text);
+        }
+        catch when (MarkFailed())
+        {
+            throw;
+        }
+    }
+
+    public void AssertNode(SyntaxKind kind)
+    {
+        try
+        {
+            Assert.True(_enumerator.MoveNext(), "Expected more nodes.");
+            Assert.Equal(kind, _enumerator.Current.Kind);
+            Assert.IsNotType<SyntaxToken>(_enumerator.Current);
+        }
+        catch when (MarkFailed())
+        {
+            throw;
+        }
+    }
+
+    private bool MarkFailed()
+    {
+        _hasErrors = true;
+        return false;
+    }
+}
---
+++ FlorianAlbert.MySharp.Sdk.Parser.Test/CodeAnalysis/Syntax/ParserTests.cs
@@ -0,0 +1,78 @@
+﻿using FlorianAlbert.MySharp.Sdk.Parser.CodeAnalysis.Syntax;
+
+namespace FlorianAlbert.MySharp.Sdk.Parser.Test.CodeAnalysis.Syntax;
+
+public class ParserTests
+{
+    [Theory]
+    [MemberData(nameof(GetBinaryOperatorPairs))]
+    public void Parser_BinaryExpression_HonorsPrecedences(SyntaxKind firstOperatorKind, SyntaxKind secondOperatorKind)
+    {
+        int firstOperatorPrecedence = SyntaxFacts.GetBinaryOperatorPrecedence(firstOperatorKind);
+        int secondOperatorPrecedence = SyntaxFacts.GetBinaryOperatorPrecedence(secondOperatorKind);
+
+        string firstOperatorText = SyntaxFacts.GetText(firstOperatorKind)!;
+        string secondOperatorText = SyntaxFacts.GetText(secondOperatorKind)!;
+
+        string text = $"a {firstOperatorText} b {secondOperatorText} c";
+
+        ExpressionSyntax expression = SyntaxTree.Parse(text).Root;
+
+        if (firstOperatorPrecedence >= secondOperatorPrecedence)
+        {
+            //     op2
+            //    /   \
+            //   op1   c
+            //  /   \
+            // a     b
+
+            using AssertingEnumerator enumerator = new(expression);
+
+            enumerator.AssertNode(SyntaxKind.BinaryExpression);
+            enumerator.AssertNode(SyntaxKind.BinaryExpression);
+            enumerator.AssertNode(SyntaxKind.NameExpression);
+            enumerator.AssertToken(SyntaxKind.IdentifierToken, "a");
+            enumerator.AssertToken(firstOperatorKind, firstOperatorText);
+            enumerator.AssertNode(SyntaxKind.NameExpression);
+            enumerator.AssertToken(SyntaxKind.IdentifierToken, "b");
+            enumerator.AssertToken(secondOperatorKind, secondOperatorText);
+            enumerator.AssertNode(SyntaxKind.NameExpression);
+            enumerator.AssertToken(SyntaxKind.IdentifierToken, "c");
+        }
+        else
+        {
+            //     op1
+            //    /   \
+            //   a    op2
+            //        /  \
+            //       b    c
+
+            using AssertingEnumerator enumerator = new(expression);
+
+            enumerator.AssertNode(SyntaxKind.BinaryExpression);
+            enumerator.AssertNode(SyntaxKind.NameExpression);
+            enumerator.AssertToken(SyntaxKind.IdentifierToken, "a");
+            enumerator.AssertToken(firstOperatorKind, firstOperatorText);
+            enumerator.AssertNode(SyntaxKind.BinaryExpression);
+            enumerator.AssertNode(SyntaxKind.NameExpression);
+            enumerator.AssertToken(SyntaxKind.IdentifierToken, "b");
+            enumerator.AssertToken(secondOperatorKind, secondOperatorText);
+            enumerator.AssertNode(SyntaxKind.NameExpression);
+            enumerator.AssertToken(SyntaxKind.IdentifierToken, "c");
+        }
+    }
+
+    public static TheoryData<SyntaxKind, SyntaxKind> GetBinaryOperatorPairs()
+    {
+        IEnumerable<SyntaxKind> binaryOperatorKinds = SyntaxFacts.GetBinaryOperatorKinds();
+        TheoryData<SyntaxKind, SyntaxKind> data = [];
+        foreach (SyntaxKind firstOperatorKind in binaryOperatorKinds)
+        {
+            foreach (SyntaxKind secondOperatorKind in binaryOperatorKinds)
+            {
+                data.Add(firstOperatorKind, secondOperatorKind);
+            }
+        }
+        return data;
+    }
+}
