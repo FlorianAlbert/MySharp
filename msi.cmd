3faa957dc9fbe0df8db73564663ebbb95cff8919
FlorianAlbert <61190722+FlorianAlbert@users.noreply.github.com>
2026-03-13 22:30:22 +0100

    Update dotnet run project path to include Interpreter subdir

--- msi.cmd
+++ msi.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-dotnet run --project .\FlorianAlbert.MySharp.Interpreter\FlorianAlbert.MySharp.Interpreter.csproj
+dotnet run --project .\Interpreter\FlorianAlbert.MySharp.Interpreter\FlorianAlbert.MySharp.Interpreter.csproj
